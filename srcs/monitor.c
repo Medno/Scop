355ff84ef67edf81c6f0dad6b8dd7572950eb962
Philippe Chadenier <phil.chadenier@gmail.com>
2019-09-10 19:14:58 +0200

    Edit comment

--- srcs/monitor.c
+++ srcs/monitor.c
@@ -6,11 +6,18 @@
 /*   By: pchadeni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 17:03:00 by pchadeni          #+#    #+#             */
-/*   Updated: 2019/09/10 17:20:43 by pchadeni         ###   ########.fr       */
+/*   Updated: 2019/09/10 18:27:58 by pchadeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "monitor.h"
+
+/*
+** Initialize SDL window
+** Double buffering for OpenGL
+** Creation of a window
+** Creation of a OpenGL context
+*/
 
 uint8_t	init_monitor(t_monitor *monitor)
 {
@@ -40,6 +47,12 @@ uint8_t	init_monitor(t_monitor *monitor)
 	}
 	return (1);
 }
+
+/*
+** // Version d'OpenGL
+** SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+** SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+*/
 
 void	destroy_monitor(t_monitor *monitor)
 {
