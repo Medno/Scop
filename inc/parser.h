255649848c79575b8e47c981011bd0d6f258fd96
Philippe Chadenier <phil.chadenier@gmail.com>
2019-10-12 20:18:33 +0200

    Parse float values

--- inc/parser.h
+++ inc/parser.h
@@ -1,4 +1,16 @@
 #ifndef PARSER_H
 # define PARSER_H
 
+enum	e_token_obj
+{
+	VERTEX,
+	TEXTURE,
+	N_VERTEX,
+	FACES,
+	NAME,
+	GROUP,
+	MATERIAL,
+	COMMENT
+};
+
 #endif
--- srcs/parser.c
+++ srcs/parser.c
@@ -1,3 +1,48 @@
 #include "parser.h"
 
-parse_obj_file(t_monitor)
+
+
+uint8_t	check_float(const char *str, float f)
+{
+	size_t	len;
+	size_t	i;
+	uint8_t	point;
+	uint8_t	is_digit;
+
+	i = 0;
+	point = 0;
+	len = ft_strlen(str);
+	while (i < len)
+	{
+		is_digit = ft_isdigit(str[i]);
+		if (!point && str[i] == '.' && i != 0)
+			point = 1;
+		else if (str[i] == '.' && point)
+			return (0);
+		else if (!is_digit)
+			return (0);
+		i++;
+	}
+	if (!point)
+		return (0);
+	f = strtod(str);
+	if (errno == ERANGE)
+		return (0);
+	return (1);
+}
+
+void	parse_obj_file(const char *obj_name)
+{
+	char	*obj_data_str;
+	char	**obj_data;
+
+	if (!(obj_data_str = read_file(obj_name, "r")))
+		return ;
+	if ((obj_data = ft_strsplit(obj_data_str, '\n')) != NULL)
+	{
+		
+		ft_tabdel(obj_data);
+	}
+	ft_strdel(&obj_data_str);
+	return ;
+}
