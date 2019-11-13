d41ae053fe8947838d694315f0b9aafd063b4c15
Philippe Chadenier <phil.chadenier@gmail.com>
2019-11-13 20:14:36 +0100

    Add Speed modifier

--- srcs/callbacks.c
+++ srcs/callbacks.c
@@ -28,6 +28,34 @@ void	scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
 		mon->camera->fov = 45.0f;
 }
 
+void	move_obj(t_monitor *mon, int key, int act)
+{
+	if (key == GLFW_KEY_K && (act == GLFW_PRESS || act == GLFW_REPEAT)
+		&& mon && mon->transformation)
+		mon->transformation->rotation.y -= 0.01f;
+	else if (key == GLFW_KEY_L && (act == GLFW_PRESS || act == GLFW_REPEAT)
+		&& mon && mon->transformation)
+		mon->transformation->rotation.y += 0.01f;
+	else if (key == GLFW_KEY_O && (act == GLFW_PRESS || act == GLFW_REPEAT)
+		&& mon && mon->transformation)
+		mon->transformation->position.y += 0.01f;
+	else if (key == GLFW_KEY_PERIOD && (act == GLFW_PRESS || act == GLFW_REPEAT)
+		&& mon && mon->transformation)
+		mon->transformation->position.y -= 0.01f;
+}
+
+void	edit_speed(t_monitor *mon, int key, int act)
+{
+	if (key == GLFW_KEY_LEFT_BRACKET && (act == GLFW_PRESS
+		|| act == GLFW_REPEAT)
+		&& mon && mon->camera && mon->camera->speed < 1.0f)
+		mon->camera->speed += 0.02;
+	else if (key == GLFW_KEY_RIGHT_BRACKET && (act == GLFW_PRESS
+		|| act == GLFW_REPEAT)
+		&& mon && mon->camera && mon->camera->speed > 0.05f)
+		mon->camera->speed -= 0.02;
+}
+
 void	key_callback(GLFWwindow *w, int key, int scancode, int act, int mods)
 {
 	t_monitor	*mon;
@@ -41,18 +69,8 @@ void	key_callback(GLFWwindow *w, int key, int scancode, int act, int mods)
 	edit_rasterization(key, act);
 	edit_texture(mon, key, act);
 	edit_rotation_activation(mon, key, act);
+	edit_speed(mon, key, act);
+	move_obj(mon, key, act);
 	if (key == GLFW_KEY_ESCAPE && act == GLFW_PRESS)
 		glfwSetWindowShouldClose(w, GLFW_TRUE);
-	else if (key == GLFW_KEY_K && (act == GLFW_PRESS || act == GLFW_REPEAT)
-		&& mon && mon->transformation)
-		mon->transformation->rotation.y -= 0.01f;
-	else if (key == GLFW_KEY_L && (act == GLFW_PRESS || act == GLFW_REPEAT)
-		&& mon && mon->transformation)
-		mon->transformation->rotation.y += 0.01f;
-	else if (key == GLFW_KEY_O && (act == GLFW_PRESS || act == GLFW_REPEAT)
-		&& mon && mon->transformation)
-		mon->transformation->position.y += 0.01f;
-	else if (key == GLFW_KEY_PERIOD && (act == GLFW_PRESS || act == GLFW_REPEAT)
-		&& mon && mon->transformation)
-		mon->transformation->position.y -= 0.01f;
 }
